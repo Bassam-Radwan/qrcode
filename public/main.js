5d04f4674d2fd936fe2da96c3b0669f2b920d3fc
Bassam-Radwan <bradwanbassam@gmail.com>
2025-08-25 12:02:36 +0100

    add a download button

--- public/main.js
+++ public/main.js
@@ -1,11 +1,28 @@
 let year = document.getElementById('year')
 let url = document.getElementById('url')
 let submit = document.getElementById('submit')
+let unValid = document.getElementById('valid')
+let loading = document.getElementById('converting')
+let main = document.getElementById('main')
+
+
 let date = new Date();
 let newYear = date.getFullYear()
 year.innerText = newYear
-
+function check(url) {
+    try {
+        let myUrl = new URL(url)
+        return true 
+    } catch {
+        unValid.style.display = 'block' 
+        return false
+    }
+}
 submit.addEventListener('click', () => {
+    if (check(url.value)) {
+        loading.style.display = 'block'
+        main.style.filter = 'blur(5px)'
+    }
     if (url.value == '') {
         alert('Please Enter a URL...')
     }
